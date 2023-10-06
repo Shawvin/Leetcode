5d4efdc4c83c303f33efb4b5ef61553a3bdbb35a
Shawvin <wxyshawvin@hotmail.com>
2023-10-07 00:03:51 +0800

    solved leetcode343

---
+++ 343-Medium.py
@@ -0,0 +1,22 @@
+## 343. Integer Break
+
+## Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the product of those integers.
+## Return the maximum product you can get.
+
+def integerBreak(n):
+    result=[0]*(n+1)
+    result[0]=1
+    result[1]=1
+    for i in range(2,n+1):
+        local_max=result[i-1]
+        for j in range(2,i):
+            temp=max(j, result[j])*max(i-j, result[i-j])
+            if temp>local_max:
+                local_max=temp
+        result[i]=local_max
+        print("{}:{}".format(i, result[i]))
+    return result[n]
+
+if __name__=='__main__':
+    n=12
+    print(integerBreak(n))    
